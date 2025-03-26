d4b13c1d162e507b362baf02598e58c9e642e6e0
esneider-77 <practicante.it1@oktaxiatlanta.com>
2025-03-26 16:06:38 -0500

    disabled gallery

--- SSR/static/PagInicio/JSPagInicio/gallery.js
+++ SSR/static/PagInicio/JSPagInicio/gallery.js
@@ -1,11 +1,11 @@
-fetch("https://graph.instagram.com/me/media?fields=media_url&access_token=IGQWRNTU85Q2ExLWNCZAjc1ZAFVjMklvd3ZABY2Y5bjJOMFpZASDJ3RjE4R092ZAk8xSmxqNGdrRmV3cHNBQ2ZAxVXByWlZARVlM4NWhXUEpPaHAtYjNjWXRha1IxQ2RUZAU50X1JPQ3VPbGJKTG8tbEV3WHdiRHBVcGF4bFUZD").then( response=>{
-    response.json().then( body=>{
-        let photos=document.querySelector("#fotos");
-        for(let url of body.data){
-            let img=document.createElement("img");
-            img.src=url.media_url;
-            img.alt="Imagen";
-            photos.appendChild(img);
-        }
-    } );
-} );
+// fetch("/disabled").then( response=>{
+//     response.json().then( body=>{
+//         let photos=document.querySelector("#fotos");
+//         for(let url of body.data){
+//             let img=document.createElement("img");
+//             img.src=url.media_url;
+//             img.alt="Imagen";
+//             photos.appendChild(img);
+//         }
+//     } );
+// } );
